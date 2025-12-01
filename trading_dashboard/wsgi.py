f4e7f05ce6ec054f1d58a73d53eff186671e5a35
vivekanandan-kumar <45902793+vivekanandan-kumar@users.noreply.github.com>
2025-12-01 11:58:06 +0000

    Add Gunicorn command to WSGI file

--- trading_dashboard/wsgi.py
+++ trading_dashboard/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trading_dashboard.settings")
 
 application = get_wsgi_application()
+gunicorn Tradeshelf.wsgi:application
